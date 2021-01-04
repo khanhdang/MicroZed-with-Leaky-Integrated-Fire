e7b5b1d55bc25ebeabb2c357ae693644ebc9127e
Khanh N. Dang <khanhdang@users.noreply.github.com>
2021-01-04 17:32:45 +0900

    update script

--- build.tcl
+++ build.tcl
@@ -88,7 +88,72 @@ current_run -implementation [get_runs impl_1]
 puts "INFO: Project created:MicroZed_NASH_SYSTEM"
 
 # Create block design
-source $origin_dir/src/bd/design_1.tcl
+################################################################
+# Block diagram build script
+################################################################
+set design_name design_1
+
+# CHECKING IF PROJECT EXISTS
+if { [get_projects -quiet] eq "" } {
+   puts "ERROR: Please open or create a project!"
+   return 1
+}
+
+set cur_design [current_bd_design -quiet]
+set list_cells [get_bd_cells -quiet]
+
+create_bd_design $design_name
+
+current_bd_design $design_name
+
+set parentCell [get_bd_cells /]
+
+# Get object for parentCell
+set parentObj [get_bd_cells $parentCell]
+if { $parentObj == "" } {
+   puts "ERROR: Unable to find parent cell <$parentCell>!"
+   return
+}
+
+# Make sure parentObj is hier blk
+set parentType [get_property TYPE $parentObj]
+if { $parentType ne "hier" } {
+   puts "ERROR: Parent <$parentObj> has TYPE = <$parentType>. Expected to be <hier>."
+   return
+}
+
+# Save current instance; Restore later
+set oldCurInst [current_bd_instance .]
+
+# Set parent object as current
+current_bd_instance $parentObj
+
+# Add the Processor System and apply board preset
+startgroup
+create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7 processing_system7_0
+endgroup
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" }  [get_bd_cells processing_system7_0]
+
+# Configure the PS: Generate 100MHz clock, Enable GP0
+startgroup
+set_property -dict [list CONFIG.PCW_USE_M_AXI_GP0 {1}] [get_bd_cells processing_system7_0]
+endgroup
+
+# Connect the FCLK_CLK0 to the PS GP0
+connect_bd_net [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK]
+
+# Add the multiplier
+startgroup
+create_bd_cell -type ip -vlnv xilinx.com:user:NASH_SYSTEM:1.0 NASH_SYSTEM_0
+endgroup
+
+# Connection automation
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "Auto" }  [get_bd_intf_pins NASH_SYSTEM_0/S00_AXI]
+
+# Restore current instance
+current_bd_instance $oldCurInst
+
+save_bd_design
 
 # Generate the wrapper
 set design_name [get_bd_designs]
