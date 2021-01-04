31a5ea3a5ef1294e0fa37029f1a4e6f13d2f32e3
Khanh N. Dang <dnk0904@gmail.com>
2021-01-04 15:38:47 +0700

    Update NASH_SYSTEM_LIF_neuron_test.c

--- ip_repo/NASH_SYSTEM_1.0/drivers/NASH_SYSTEM_v1_0/src/NASH_SYSTEM_LIF_neuron_test.c
+++ ip_repo/NASH_SYSTEM_1.0/drivers/NASH_SYSTEM_v1_0/src/NASH_SYSTEM_LIF_neuron_test.c
@@ -13,7 +13,7 @@ int r;
 int V;
 xil_printf("NASH Test\n\r");
 
-// Write multiplier inputs to register 0
+// Write LIF input to register 0
 *(baseaddr_p+0) = 0x00010000; // reset
 
 for (int i=0; i < 10; i++) {
@@ -32,7 +32,7 @@ for (int i=0; i < 10; i++) {
 	}
 	*(baseaddr_p+0) = 0x00040000; //
 	xil_printf("Wrote: 0x%08x \n\r", *(baseaddr_p+0));
-	// Read multiplier output from register 1
+	// Read LIF output from register 1
 	xil_printf("Read : 0x%08x \n\r", *(baseaddr_p+1));
 	xil_printf("End of time-step\n\n\r");
 }
